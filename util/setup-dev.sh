068a45f916efd8ec56f779f21c26ae9e3a972c97
NormanBenbrahim <norman.benbrahim@gmail.com>
2020-11-03 14:19:03 -0500

    update only

--- util/setup-dev.sh
+++ util/setup-dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # comment this line out after running it the first time
-sudo apt update && sudo apt upgrade --fix-missing
+sudo apt update --fix-missing
 
 if [ ! -d "venv" ]; then
     echo ""
