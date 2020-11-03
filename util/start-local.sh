fc97f55f1bc0337f9fbbcb7b92ea296d427600b7
NormanBenbrahim <norman.benbrahim@gmail.com>
2020-11-03 14:12:47 -0500

    first

---
+++ util/start-local.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# set the google cloud project
+gcloud config set project watch-mind-med
+
+if [ ! -d "venv" ]; then
+    echo ""
+    echo "Local virtual environment not found, creating..."
+    virtualenv venv
+fi
+
+# some housekeeping 
+if [ ! -z "${VIRTUAL_ENV}" ]; then
+    pip install -r requirements.txt
+else
+    echo ""
+    echo "Please type 'source venv/bin/activate' first, then re-run this file"
+    exit
+fi
+
+# create env variable letting the app know it is running a local instance
+export IS_LOCAL="0"
+export IS_DEV="1"
+export IS_PROD="0"
+
+gcloud app deploy app.yaml
