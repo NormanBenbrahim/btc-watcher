2f3751c28fff2ceef7bd9743dce8cac2037f6421
NormanBenbrahim <norman.benbrahim@gmail.com>
2020-11-08 21:28:22 -0500

    download routes now make the right calls

--- api/routers/download.py
+++ api/routers/download.py
@@ -6,24 +6,57 @@ import requests
 
 router = APIRouter()
 
-# get time objects
-time_now = datetime.now(timezone('EST'))
-time_three_months_ago = time_now + relativedelta(months=-3)
-
 @router.get('/download')
-async def download():
+async def download_last_3_months():
     """
-    Route to download the last 3 months of bitcoin data to google cloud 
+    Route to download the last 3 months of bitcoin data to google cloud
+    with hourly tickers
+
+    RETURNS
+        JSON object containing collection of [market cap, price]
     """
-    #response = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=cad')
-    #response.raise_for_status()
-    return {200: "done"}
+    # get timestamps
+    time_now = datetime.now(timezone('EST'))
+    time_three_months_ago = time_now + relativedelta(months=-3)
+
+    # build request url
+    base_url = 'https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=cad&'
+    endpoint_url = f"from={time_three_months_ago.timestamp()}&to={time_now.timestamp()}"
+    request_url = base_url + endpoint_url 
+    
+    # handle response
+    response = requests.get(request_url)
+    response.raise_for_status()
+    
+    # spit it back
+    return response.json()
 
 
-@router.get('/download/{months}')
-async def download_last_months(months):
+@router.get('/download/{n_months}')
+async def download_last_X_months(n_months: int):
     """
-    Route to download the last {X} months of bitcoin data to google cloud 
-    """
+    Route to download the last {n_months} months of bitcoin data to google cloud
+    with hourly tickers
 
-    return {200: str(months)}
+    PARAMS 
+        months: number of months to go backwards (keep in mind cryptocurrencies were created
+                in 2009)
+
+    RETURNS
+        JSON object containing collection of [market cap, price]
+    """
+    # get timestamps
+    time_now = datetime.now(timezone('EST'))
+    time_three_months_ago = time_now + relativedelta(months=-n_months)
+
+    # build request url
+    base_url = 'https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=cad&'
+    endpoint_url = f"from={time_three_months_ago.timestamp()}&to={time_now.timestamp()}"
+    request_url = base_url + endpoint_url 
+    
+    # handle response
+    response = requests.get(request_url)
+    response.raise_for_status()
+    
+    # spit it back
+    return response.json()
